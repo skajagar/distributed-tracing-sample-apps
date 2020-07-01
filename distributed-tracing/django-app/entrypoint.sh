7c3f3aebfce7fe2f0ee76dc9e0295a9815df217d
skajagar <33450440+skajagar@users.noreply.github.com>
2020-07-01 13:10:33 +0530

    Update entrypoint.sh

--- distributed-tracing/django-app/entrypoint.sh
+++ distributed-tracing/django-app/entrypoint.sh
@@ -3,7 +3,7 @@ enableProxy=$1
 proxyIPOrDIToken=$2
 applicationName=$3
 flushInterval=$4
-python3 ./shopping/manage.py runserver 50050 "$enableProxy" "$proxyIPOrDIToken" "$applicationName"
-python3 ./styling/manage.py runserver 50051 "$enableProxy" "$proxyIPOrDIToken" "$applicationName"
-python3 ./styling/manage.py runserver 50052 "$enableProxy" "$proxyIPOrDIToken" "$applicationName"
+python3 ./shopping/manage.py runserver 50050 "$enableProxyEnv" "$proxyIPOrWFClustterEnv" "$ProxyPortorWFTokenEnv" "$applicationNameEnv"
+python3 ./styling/manage.py runserver 50051 "$enableProxyEnv" "$proxyIPOrWFClustterEnv" "$ProxyPortorWFTokenEnv" "$applicationNameEnv"
+python3 ./styling/manage.py runserver 50052 "$enableProxyEnv" "$proxyIPOrWFClustterEnv" "$ProxyPortorWFTokenEnv" "$applicationNameEnv"
 ./loadgen.sh "$flushInterval"
