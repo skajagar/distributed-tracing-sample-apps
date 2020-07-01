27bc46c2fd3d767baa056e2a2596b190a289b27b
skajagar <33450440+skajagar@users.noreply.github.com>
2020-07-01 17:25:14 +0530

    Update entrypoint.sh

--- distributed-tracing/django-app/entrypoint.sh
+++ distributed-tracing/django-app/entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 enableProxy=$1
-proxyIPOrDIToken=$2
-applicationName=$3
-flushInterval=$4
-python3 ./shopping/manage.py runserver 50050 "$enableProxyEnv" "$proxyIPOrWFClustterEnv" "$ProxyPortorWFTokenEnv" "$applicationNameEnv"
-python3 ./styling/manage.py runserver 50051 "$enableProxyEnv" "$proxyIPOrWFClustterEnv" "$ProxyPortorWFTokenEnv" "$applicationNameEnv"
-python3 ./delivery/manage.py runserver 50052 "$enableProxyEnv" "$proxyIPOrWFClustterEnv" "$ProxyPortorWFTokenEnv" "$applicationNameEnv"
+proxyIPOrWFClustter=$2
+ProxyPortorWFToken$3
+applicationName=$4
+flushInterval=$5
+python3 ./shopping/manage.py runserver 50050 "$enableProxyEnv" "$proxyIPOrWFClustterEnv" "$ProxyPortorWFTokenEnv" "$applicationNameEnv" &
+python3 ./styling/manage.py runserver 50051 "$enableProxyEnv" "$proxyIPOrWFClustterEnv" "$ProxyPortorWFTokenEnv" "$applicationNameEnv" &
+python3 ./delivery/manage.py runserver 50052 "$enableProxyEnv" "$proxyIPOrWFClustterEnv" "$ProxyPortorWFTokenEnv" "$applicationNameEnv" &
 ./loadgen.sh "$flushInterval"
