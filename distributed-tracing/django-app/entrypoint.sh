cb9dff05e65efc6ec9dfb8818bd240e48485d43f
skajagar <skajagar@vmware.com>
2020-07-06 14:29:51 +0530

    create wavefront client based on the enableproxy flag

--- distributed-tracing/django-app/entrypoint.sh
+++ distributed-tracing/django-app/entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 enableProxy=$1
 proxyIPOrWFClustter=$2
-ProxyPortorWFToken$3
+ProxyPortorWFToken=$3
 applicationName=$4
 flushInterval=$5
-python3 ./shopping/manage.py runserver 50050 "$enableProxyEnv" "$proxyIPOrWFClustterEnv" "$ProxyPortorWFTokenEnv" "$applicationNameEnv" &
-python3 ./styling/manage.py runserver 50051 "$enableProxyEnv" "$proxyIPOrWFClustterEnv" "$ProxyPortorWFTokenEnv" "$applicationNameEnv" &
-python3 ./delivery/manage.py runserver 50052 "$enableProxyEnv" "$proxyIPOrWFClustterEnv" "$ProxyPortorWFTokenEnv" "$applicationNameEnv" &
+
+python3 ./shopping/manage.py runserver 50050 "$enableProxy" "$proxyIPOrWFClustter" "$ProxyPortorWFToken" "$applicationName" &
+python3 ./styling/manage.py runserver 50051 "$enableProxy" "$proxyIPOrWFClustter" "$ProxyPortorWFToken" "$applicationName" &
+python3 ./delivery/manage.py runserver 50052 "$enableProxy" "$proxyIPOrWFClustter" "$ProxyPortorWFToken" "$applicationName" &
 ./loadgen.sh "$flushInterval"
